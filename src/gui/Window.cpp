c006e2dadaeb20c68b6663ada1b7e3a03b45c542
jiang.wenqiang <jiang.wenqiang@byd.com>
2018-07-19 20:19:24 +0800

    能够初步实现布局和一些响应

--- src/gui/Window.cpp
+++ src/gui/Window.cpp
@@ -7,6 +7,11 @@
 #include <QtWidgets/QMenu>
 #include <QtWidgets/QAction>
 #include <QtWidgets/QStatusBar>
+#include <QtWidgets/QTabWidget>
+#include <QtWidgets/QVBoxLayout>
+
+#include <QtCore/QDebug>
+#include <QtWidgets/QApplication>
 
 Window::Window(QWidget *parent) : QMainWindow(parent) {
     setup_ui();
@@ -15,24 +20,88 @@ Window::Window(QWidget *parent) : QMainWindow(parent) {
 void Window::setup_ui() {
     if (this->objectName().isEmpty())
         this->setObjectName(QStringLiteral("MainWindow"));
+    
+    this->setDocumentMode(true);
     this->setWindowTitle(QString("Refine匹配软件"));
+    this->setWindowState(Qt::WindowMaximized);
+    central_widget = new QWidget(this);
+    this->setCentralWidget(central_widget);
+    whole_hbox_layout = new QHBoxLayout(central_widget);
+    mid_widget = new QWidget(this);
+    mid_vbox_layout = new QVBoxLayout(mid_widget);
+    
+    opengl = new QOpenGLWidget(mid_widget);
+    
+    central_widget->setLayout(whole_hbox_layout);
+    left_tab_widget = new QTabWidget(this);
+    left_tab_widget->addTab(new QWidget(this), QString("匹配"));
+    left_tab_widget->addTab(new QWidget(this), QString("曲线"));
+    left_tab_widget->addTab(new QWidget(this), QString("工况"));
+    left_tab_widget->setTabEnabled(0, false);
+    left_tab_widget->setTabBarAutoHide(true);
+    left_tab_widget->setTabPosition(QTabWidget::West);
+    left_tab_widget->setTabBarAutoHide(true);
+    whole_hbox_layout->addWidget(left_tab_widget,1);
+//    whole_hbox_layout->setMargin(0);
+    whole_hbox_layout->addWidget(mid_widget,7);
+
+    mid_widget->setLayout(mid_vbox_layout);
+    mid_vbox_layout->addWidget(opengl,10);
+    mid_vbox_layout->setMargin(0);
+    opengl_scroller = new QScrollBar(mid_widget);
+    opengl_scroller->setOrientation(Qt::Horizontal);
+    mid_vbox_layout->addWidget(opengl_scroller, 1);
+
     menu_bar = new QMenuBar(this);
     menu_bar->setObjectName(QStringLiteral("menu_bar"));
-    menu = new QMenu(QString("文件"), menu_bar);
-    menu->setObjectName(QStringLiteral("menu"));
     menu_bar->setGeometry(QRect(0, 0, 300, 23));
+
+    menu_file = new QMenu(QString("文件(&F)"), menu_bar);
+    menu_setting = new QMenu(QString("设置(&S)"));
+    menu_file->setObjectName(QStringLiteral("menu"));
+    menu_action_new = new QAction(this);
+    menu_action_new->setObjectName(QStringLiteral("menu_action_file"));
+    menu_action_new->setText("新建(&N)");
+    menu_setting_skin = new QAction(this);
+    menu_setting_skin->setText("皮肤(&S)");
+
+    menu_file->addAction(menu_action_new);
+    menu_setting->addAction(menu_setting_skin);
+
+    menu_bar->addAction(menu_file->menuAction());
+    menu_bar->addAction(menu_setting->menuAction());
+
     this->setMenuBar(menu_bar);
     status_bar = new QStatusBar(this);
     status_bar->setObjectName(QStringLiteral("status_bar"));
+    status_bar->addAction(menu_action_new);
+
     this->setStatusBar(status_bar);
+
+    connect(menu_setting_skin, &QAction::triggered, this, &Window::change_skin);
 }
 
-void Window::break_ui() {
-    delete menu;
-    delete menu_bar;
-    delete status_bar;
+Window::~Window() {}
+
+void Window::resizeEvent(QResizeEvent *event) {
+    qDebug() << this->size();
 }
 
-Window::~Window() {
-    break_ui();
+void Window::change_skin() {
+    static int flag = 0;
+    flag += 1;
+    flag %= 3;
+    switch (flag) {
+        case 0:
+            QApplication::setStyle("fusion");
+            break;
+        case 1:
+            QApplication::setStyle("windows");
+            break;
+        case 2:
+            QApplication::setStyle("windowsvista");
+            break;
+        default:
+            break;
+    }
 }
--- src/gui/Window.h
+++ src/gui/Window.h
@@ -6,19 +6,40 @@
 #define REFINE_WINDOW_H
 
 #include <QtWidgets/QMainWindow>
+#include <QtWidgets/QHBoxLayout>
+#include <QtWidgets/QOpenGLWidget>
+#include <QtWidgets/QScrollBar>
 
 class Window : public QMainWindow{
 Q_OBJECT
 private:
+    QWidget *central_widget;
     QMenuBar *menu_bar;
-    QMenu *menu;
+    QMenu *menu_file;
+    QMenu *menu_setting;
     QStatusBar *status_bar;
+    QAction *menu_action_new;
+    QAction *menu_setting_skin;
+    QHBoxLayout *whole_hbox_layout;
+    QVBoxLayout *mid_vbox_layout;
+    QWidget *mid_widget;
+    QOpenGLWidget *opengl;
+    QScrollBar *opengl_scroller;
+
+
+    QTabWidget *left_tab_widget;
+
 public:
     explicit Window(QWidget *parent = nullptr);
     ~Window() final;
 
     void setup_ui();
-    void break_ui();
+
+    void resizeEvent(QResizeEvent *event) Q_DECL_OVERRIDE;
+
+public slots:
+    void change_skin();
+
 };
 
 
